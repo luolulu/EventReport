29a6df242d883624a61664dd392d513001229ad2
luolulu <luo@LUOdeMacBook-Pro.local>
2018-07-08 12:34:48 -0500

    add fragment

--- app/src/main/java/com/laioffer/eventreporter/MainActivity.java
+++ app/src/main/java/com/laioffer/eventreporter/MainActivity.java
@@ -1,28 +1,47 @@
 package com.laioffer.eventreporter;
 
+import android.content.res.Configuration;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.ListView;
 
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements EventFragment.OnItemSelectListener {
+
+    private EventFragment mListFragment;
+    private CommentFragment mGridFragment;
+    @Override
+    public void onItemSelected(int position) {
+        mGridFragment.onItemSelected(position);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //add list view
+        mListFragment = new EventFragment();
+        getSupportFragmentManager().beginTransaction().replace(R.id.event_container, mListFragment).commit();
 
 
-        @Override
-        protected void onCreate(Bundle savedInstanceState) {
-            super.onCreate(savedInstanceState);
-            setContentView(R.layout.activity_main);
+        //add Gridview
+        if (isTablet()) {
+            mGridFragment = new CommentFragment();
+            getSupportFragmentManager().beginTransaction().replace(R.id.comment_container, mGridFragment).commit();
 
-            // Get ListView object from xml.
-            ListView eventListView = (ListView) findViewById(R.id.event_list);
-
-
-            EventAdapter adapter = new EventAdapter(this);
-
-            // Assign adapter to ListView.
-            eventListView.setAdapter(adapter);
         }
+    }
+
+    private boolean isTablet() {
+        return (getApplicationContext().getResources().getConfiguration().screenLayout &
+                Configuration.SCREENLAYOUT_SIZE_MASK) >=
+                Configuration.SCREENLAYOUT_SIZE_LARGE;
+    }
+//xml boolean value depends on screen size
+
+}
 
 
- }
 
